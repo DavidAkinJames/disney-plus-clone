9b35f672271a4cfc2ef24a43000883e86d824165
David Akin-James <davidakinjames@yahoo.com>
2021-05-21 20:08:00 +0100

    Added the Detail Components

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,45 +1,66 @@
-import React from 'react'
-import styled from 'styled-components'
 
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+import db from '../firebase'
 
 
 
 
 function Detail() {
-  return (
-    <Container>
-      <Background>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
-      </Background>
-      <ImageTitle>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt="" />
-      </ImageTitle>
-      <Controls>
-         <PlayButton>
-             <img src="/images/play-icon-black.png" alt="" />
-             <span>PLAY</span>
-         </PlayButton>
-         <TrailerButton>
-         <img src="/images/play-icon-white.png" alt=""/>
-         <span>Trailer</span>
-         </TrailerButton>
-         <AddButton>
-            <span>+</span>
-         </AddButton>
-         <GroupWatchButton>
-           <img src="/images/group-icon.png" alt=""/>
-         </GroupWatchButton>
-      </Controls>
-      <SubTitle>
-        2018. 7m Family, Fantasy, Kids, Animation
-      </SubTitle>
-      <Description>
-        Lorem Ipsum Movie
-      </Description>
-    </Container>
-  )
-}
+    const {id} = useParams();
+    const [ movie, setMovie ] = useState();
+   
 
+    useEffect(()=> {
+       //Grab the movie from DB
+       db.collection('movies')
+       .doc(id)
+       .get()
+       .then((doc)=> {
+         if (doc.exists) {
+             //save the movie data
+            setMovie(doc.data());
+
+         } else {
+           //redirects to homepage
+         }
+       })
+    }, [id])
+   
+    return (
+        <Container>
+            <Background>
+                 <img src={movie.backgroundImg} alt=""/> 
+            </Background>
+            <ImageTitle>
+                <img src={movie.titleImg} alt="" />
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="/images/play-icon-black.png" alt="" />
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>s
+                    <img src="/images/play-icon-white.png" alt=""/>
+                    <span>Trailer</span>
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="/images/group-icon.png" alt=""/>
+                </GroupWatchButton>
+            </Controls>
+            <SubTitle>
+               {movie.subTitle}
+            </SubTitle>
+            <Description>
+               {movie.description}
+            </Description>
+        </Container>
+    )
+}
 export default Detail
 
 
